2f2c1db6f38fe467f92c0564236bb9589a94c540
umer3636vp <umer3636vp@gmail.com>
2026-02-05 20:49:58 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function setBg(type) {
+  if (type === 'purple') {
+    document.body.style.background =
+      'linear-gradient(135deg, #4c1d95, #1e1b4b)';
+  }
+  if (type === 'dark') {
+    document.body.style.background =
+      'linear-gradient(135deg, #020617, #020617)';
+  }
+}
